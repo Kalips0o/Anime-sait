ea938efa0a5fab7868290b80e8e7610f218cd5ec
Kalips0o <Kalipsoo92@gmail.com>
2023-07-31 23:13:36 +0300

    new img

--- js/categoriesData.js
+++ js/categoriesData.js
@@ -1,28 +1,39 @@
+// Определение функции categoriesData
 const categoriesData = () => {
+    // Находим элемент с классом "preloader" и сохраняем его в переменную preloader
     const preloader = document.querySelector('.preloder');
+
+    // Функция для отображения списка жанров в выпадающем меню
     const renderGanreList = (ganres) => {
+        // Находим элемент с классом "dropdown" внутри элемента с классом "header__menu" и сохраняем его в переменную dropdownBlock
         const dropdownBlock = document.querySelector('.header__menu .dropdown');
 
+        // Перебираем каждый жанр из массива ganres
         ganres.forEach(ganre => {
+            // Добавляем каждый жанр в список, создавая соответствующие HTML элементы с помощью insertAdjacentHTML
             dropdownBlock.insertAdjacentHTML('beforeend', `
             <li><a href="./categories.html?ganre=${ganre}">${ganre}</a></li>
          `);
         });
     }
 
-
+    // Функция для отображения списка аниме определенного жанра
     const renderAnimeList = (array, ganres) => {
+        // Находим элемент с классом "product-page" и вложенным элементом с классом "col-lg-8" и сохраняем его в переменную wrapper
         const wrapper = document.querySelector('.product-page .col-lg-8');
 
+        // Перебираем каждый жанр из массива ganres
         ganres.forEach((ganre) => {
+            // Создаем блок элементов для аниме определенного жанра
             const productBlock = document.createElement('div');
             const listBlock = document.createElement('div');
             const list = array.filter((item => item.tags.includes(ganre)));
 
-
+            // Добавляем классы стилизации для блоков
             listBlock.classList.add('row');
             productBlock.classList.add('mb-5');
 
+            // Вставляем HTML-код для блока заголовка жанра и кнопки "View All"
             productBlock.insertAdjacentHTML(
                 'beforeend',
                 `
@@ -41,15 +52,19 @@ const categoriesData = () => {
          `
             );
 
+            // Перебираем каждый элемент аниме определенного жанра
             list.forEach(item => {
+                // Создаем блок тегов для аниме
                 const tagsBlock = document.createElement('ul');
 
+                // Добавляем каждый тег в блок тегов
                 item.tags.forEach(tag => {
                     tagsBlock.insertAdjacentHTML('beforeend', `
                   <li>${tag}</li>
                `);
                 });
 
+                // Вставляем HTML-код для элемента аниме
                 listBlock.insertAdjacentHTML('beforeend', `
             <div class="col-lg-4 col-md-6 col-sm-6">
                <div class="product__item">
@@ -66,24 +81,31 @@ const categoriesData = () => {
             `);
             });
 
+            // Добавляем блок элементов аниме определенного жанра в обертку
             productBlock.append(listBlock);
             wrapper.appendChild(productBlock);
 
+            // Устанавливаем фоновое изображение для всех элементов с классом "set-bg" из их свойства "data-setbg"
             wrapper.querySelectorAll('.set-bg').forEach((elem) => {
                 elem.style.backgroundImage = `url(${elem.dataset.setbg})`;
             });
         });
 
+        // Убираем прелоадер через 500 миллисекунд (0.5 секунды)
         setTimeout(() => {
             preloader.classList.remove('active');
         }, 500);
     };
 
+    // Функция для отображения списка популярного аниме
     const renderTopAnime = (anime) => {
+        // Находим элемент с классом "filter__gallery" и сохраняем его в переменную wrapper
         const wrapper = document.querySelector('.filter__gallery');
 
+        // Очищаем содержимое элемента wrapper
         wrapper.innerHTML = ``;
 
+        // Перебираем каждый элемент популярного аниме и вставляем его HTML-код в обертку
         anime.forEach((item) => {
             wrapper.insertAdjacentHTML(
                 'beforeend',
@@ -98,31 +120,42 @@ const categoriesData = () => {
             );
         });
 
+        // Устанавливаем фоновое изображение для всех элементов с классом "set-bg" из их свойства "data-setbg"
         wrapper.querySelectorAll('.set-bg').forEach((elem) => {
             elem.style.backgroundImage = `url(${elem.dataset.setbg})`;
         });
     };
 
+    // Выполняем запрос к удаленному серверу для получения данных об аниме
     fetch('https://anime-date-fbec3-default-rtdb.firebaseio.com/anime.json')
-        .then((response) => response.json())
+        .then((response) => response.json()) // Получаем ответ сервера в формате JSON
         .then((data) => {
+            // Создаем пустой объект Set с названиями ganres для хранения уникальных жанров из данных
             const ganres = new Set();
+            // Получаем значение параметра "ganre" из текущего URL страницы
             const ganreParams = new URLSearchParams(window.location.search).get('ganre');
 
+            // Перебираем каждый элемент данных об аниме и добавляем его жанр в набор ganres
             data.forEach((item) => {
                 ganres.add(item.ganre);
             });
 
+            // Вызываем функцию renderTopAnime для отображения списка популярного аниме, отсортированного по просмотрам и ограниченного до 5 элементов
             renderTopAnime(data.sort((a, b) => b.views - a.views).slice(0, 5));
 
+            // Проверяем, есть ли параметр "ganre" в URL
             if (ganreParams) {
+                // Если параметр "ganre" есть, вызываем функцию renderAnimeList, передавая данные об аниме и массив с единственным выбранным жанром
                 renderAnimeList(data, [ganreParams]);
             } else {
+                // Если параметра "ganre" нет, вызываем функцию renderAnimeList, передавая данные об аниме и весь набор жанров
                 renderAnimeList(data, ganres);
             }
 
+            // Вызываем функцию renderGanreList для отображения списка жанров в выпадающем меню
             renderGanreList(ganres);
         });
 };
 
+// Вызываем функцию categoriesData для начала работы с данными и отображения на странице
 categoriesData();
--- js/detailData.js
+++ js/detailData.js
@@ -1,18 +1,28 @@
+// Определение функции detailData
 const detailData = () => {
+    // Находим элемент с классом "preloader" и сохраняем его в переменную preloader
     const preloader = document.querySelector('.preloder');
 
+    // Функция для отображения списка жанров в выпадающем меню
     const renderGanreList = (ganres) => {
+        // Находим элемент с классом "dropdown" внутри элемента с классом "header__menu" и сохраняем его в переменную dropdownBlock
         const dropdownBlock = document.querySelector('.header__menu .dropdown');
 
+        // Перебираем каждый жанр из массива ganres
         ganres.forEach(ganre => {
+            // Добавляем каждый жанр в список, создавая соответствующие HTML элементы с помощью insertAdjacentHTML
             dropdownBlock.insertAdjacentHTML('beforeend', `
             <li><a href="./categories.html?ganre=${ganre}">${ganre}</a></li>
          `);
         });
     }
 
+    // Функция для отображения деталей выбранного аниме
     const renderAnimeDetails = (array, itemId) => {
+        // Находим объект аниме с соответствующим id (itemId) из массива данных array и сохраняем его в переменную animeObj
         const animeObj = array.find(item => item.id === +itemId);
+
+        // Находим различные элементы на странице и сохраняем их в соответствующие переменные
         const imageBlock = document.querySelector('.anime__details__pic');
         const viewsBlock = imageBlock.querySelector('.view');
         const titleBlock = document.querySelector('.anime__details__title h3');
@@ -21,16 +31,24 @@ const detailData = () => {
         const widgetList = document.querySelectorAll('.anime__details__widget ul li');
         const breadcrumb = document.querySelector('.breadcrumb__links span');
 
-        if(animeObj) {
+        // Если аниме существует (нашли объект аниме по id)
+        if (animeObj) {
+            // Устанавливаем фоновое изображение для элемента с классом "anime__details__pic" из свойства "image" объекта animeObj
             imageBlock.dataset.setbg = animeObj.image;
+
+            // Добавляем информацию о просмотрах в блок с классом "view"
             viewsBlock.insertAdjacentHTML('beforeend', `
             <i class="fa fa-eye">${animeObj.views}</i>
          `);
 
+            // Обновляем заголовок и подзаголовок с информацией из объекта animeObj
             titleBlock.textContent = animeObj.title;
             subtitleBlock.textContent = animeObj['original-title'];
+
+            // Обновляем текстовый блок с описанием аниме
             descriptionBlock.textContent = animeObj.description;
 
+            // Обновляем виджеты с информацией о дате выпуска, рейтинге и жанрах
             widgetList[0].insertAdjacentHTML('beforeend', `
             <span>Date aired: </span>${animeObj.date}
          `);
@@ -41,38 +59,51 @@ const detailData = () => {
             <span>Genre: </span>${animeObj.tags.join(', ')}
          `);
 
+            // Обновляем хлебные крошки с информацией о жанре аниме
             breadcrumb.textContent = animeObj.ganre;
 
+            // Устанавливаем фоновое изображение для всех элементов с классом "set-bg" из их свойства "data-setbg"
             document.querySelectorAll('.set-bg').forEach((elem) => {
                 elem.style.backgroundImage = `url(${elem.dataset.setbg})`;
             });
 
+            // Убираем прелоадер через 500 миллисекунд (0.5 секунды)
             setTimeout(() => {
                 preloader.classList.remove('active');
             }, 500);
         } else {
+            // Если аниме не найдено (animeObj равен undefined), выводим сообщение "Аниме отсутствует!" в консоль
             console.log('Аниме отсутствует!');
         }
     };
 
+    // Выполняем запрос к удаленному серверу для получения данных об аниме
     fetch('https://anime-date-fbec3-default-rtdb.firebaseio.com/anime.json')
-        .then((response) => response.json())
+        .then((response) => response.json()) // Получаем ответ сервера в формате JSON
         .then((data) => {
+            // Создаем пустой объект Set с названиями ganres для хранения уникальных жанров из данных
             const ganres = new Set();
+
+            // Получаем значение параметра "itemId" из текущего URL страницы
             const ganreParams = new URLSearchParams(window.location.search).get('itemId');
 
+            // Перебираем данные и добавляем уникальные жанры в ganres
             data.forEach((item) => {
                 ganres.add(item.ganre);
             });
 
+            // Если задан параметр "itemId", вызываем функцию renderAnimeDetails с данными и ganreParams
             if (ganreParams) {
                 renderAnimeDetails(data, ganreParams);
             } else {
+                // Иначе, если параметр "itemId" не задан, выводим сообщение "Аниме отсутствует!" в консоль
                 console.log('Аниме отсутствует!');
             }
 
+            // Вызываем функцию renderGanreList с полученным списком уникальных жанров
             renderGanreList(ganres);
         });
 };
 
+// Вызываем функцию detailData для выполнения всей логики при загрузке страницы
 detailData();
